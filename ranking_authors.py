b76393dd34d498dd3d7156af751c654e0cb1d19b
willimemo <38484566+willimemo@users.noreply.github.com>
2018-05-06 23:29:17 -0500

    Add files via upload

--- ranking_authors.py
+++ ranking_authors.py
@@ -13,10 +13,29 @@ def remove_duplicates(values):
     return output
 
 
-print(text)
+
 authors_list = text.split("\n")
 authors_list.remove('')
-print(authors_list)
 nodup_list = remove_duplicates(authors_list)
 print(nodup_list)
 
+#This does put a list into keys
+grammar_dict = {}
+
+committee_num = eval(input("How many committee members are there?: "))
+for k in nodup_list:
+    print("Author: " + k)
+    authors_scores = []
+    for score in range(committee_num):
+
+        score = eval(input("On a scale of 1 to 10 with 10 being the highest, how would you rate this author on grammar?: "))
+        authors_scores.append(score)
+        grammar_dict[k] = authors_scores
+print(grammar_dict)
+
+#use this to add the values in the list and print a new dictionary that has the key and its one acumulated value
+
+dict2 = {}
+for key in grammar_dict:
+    dict2[key] = sum(grammar_dict[key])
+print(dict2)
