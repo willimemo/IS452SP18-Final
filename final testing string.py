7ce028c0af03f23dd875648f8bd3ba2da351fb65
willimemo <38484566+willimemo@users.noreply.github.com>
2018-05-07 21:52:56 -0500

    Add files via upload

--- final testing string.py
+++ final testing string.py
@@ -1,22 +1,32 @@
+#This is the first python script that should be run
+#This script reads in a starting list of nominees
 in_file = open('Start_authors_string.txt', 'r')
 text = in_file.read()
 in_file.close()
 
+#this is writing the starting list into a new list
+#This is also the outfile for nominees added to the list
 out_file = open('appended_authors_string.txt', 'a')
 out_file.write(text)
 
-nominee = eval(input("Enter the name of your nominee for best author in the format of 'LastName, FirstName': "))
-if nominee in text:
-    print("Your nominee has already been nominated for best author")
+#The loop will allow the user to enter in all of the new nominees all at once
+#the if, elif and else statements will check the starting list to see if the author is already in the list or not
 
-elif nominee not in text:
-    out_file.write('\n' + nominee)
-    print("Thank you. Your nominated author has been added to the list of nominees for 'Best Author'")
-else:
-    print("This shouldn't execute")
+entries = eval(input("Enter the number of nominees you need to add to the list.: "))
+for nominee in range(entries):
 
+    nominee = eval(input("Enter the name of your nominee for best author in the format of 'LastName, FirstName': "))
+    if nominee in text:
+        print("Your nominee has already been nominated for best author")
 
+    elif nominee not in text:
+        out_file.write('\n' + nominee)
+        print("Thank you. Your nominated author has been added to the list of nominees for 'Best Author'")
+    else:
+        print("This shouldn't execute")
 
+print("Thank you. All of your nominees have been entered")
+#This print statement is only there to let the user know that they are done.
 
 
 
